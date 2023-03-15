e91ead9409d06f960e2e18bed152aca157dea61b
Debashis Nandi <debashisnandi576@gmail.com>
2023-03-16 03:52:55 +0530

    added the config file and made changes in app.py

--- app.py
+++ app.py
@@ -6,10 +6,9 @@ from PIL import Image
 
 classes=['1-1','1-2','1-4','PURE']
 
-def predict(img):
+def predictCapturedImage(img):
     model=tf.keras.models.load_model('./My_CNN_Model.h5')
     if img is not None:
-        st.image(img)
         x_=Image.open(img)
         x_=x_.resize((256,256))
         x=tf.keras.utils.img_to_array(x_)
@@ -17,7 +16,28 @@ def predict(img):
         processed_img=np.expand_dims(x,axis=0)
         pred=model.predict(processed_img)
         prediction=classes[np.argmax(pred)]
-        st.success(prediction)
+        return prediction
+    else:
+        return ""
+
+
+def predictUploadedImage(img):
+    model=tf.keras.models.load_model('./My_CNN_Model.h5')
+    if img is not None:
+        column1,column2,column3=st.columns([0.1,1,0.1])
+        column1.write(" ")
+        column2.image(img)
+        column3.write(" ")
+        x_=Image.open(img)
+        x_=x_.resize((256,256))
+        x=tf.keras.utils.img_to_array(x_)
+        x=x/255
+        processed_img=np.expand_dims(x,axis=0)
+        pred=model.predict(processed_img)
+        prediction=classes[np.argmax(pred)]
+        return prediction
+    else:
+        return ""
 
 
 def main():
@@ -44,17 +64,25 @@ def main():
     """
     st.markdown(title,unsafe_allow_html=True)
 
-    # img=Image.open('./upload_image.jpg')
-    # new_img=img.resize((300,300))
+
+    captured_img=st.camera_input("Click a picture")
+    result1=predictCapturedImage(captured_img)
     
+    img=st.file_uploader("Choose an image file",type=['jpg','png','.jpeg'])
+    result2=predictUploadedImage(img)
+
+    if (result1=="" and result2==""):
+        st.success("Please capture or select one image to predict adulreration")
+    elif (result1!="" and result2==""):
+        st.success(result1)
+    elif (result2!="" and result1==""):
+        st.success(result2)
+    else:
+        st.success(result1)
+        st.success(result2)
 
 
-    img=st.file_uploader("Choose an image",type=['jpg','png','.jpeg'])
-    st.button("Predict",key=1,on_click=predict(img))
 
-
-    captured_img=st.camera_input("Click a pic")
-    st.button("Predict",key=2,on_click=predict(captured_img))
     
 
     
